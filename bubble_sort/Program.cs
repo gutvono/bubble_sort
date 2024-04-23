66b0ce1e4191301deee3206048247cf80321a674
Gustavo <guvono4@gmail.com>
2024-04-23 16:09:10 -0300

    update: remove os numeros repetidos no momento de copiar o vetor original para o que será ordenado

--- bubble_sort/Program.cs
+++ bubble_sort/Program.cs
@@ -1,27 +1,88 @@
-﻿int[] numeros = new int[10];
-int i = 0, j = 0, temp = 0;
+﻿int aux = 0, size = 10, size_ordenado = 0, restart = 0;
+int[] original = new int[size];
+int[] ordenado = new int[size];
+int[] invertido = new int[size];
 
-Console.WriteLine("Informe os valores desejados para que sejam ordenados:");
-for (i = 0; i < 5; i++)
+do
 {
-    Console.Write(i + 1 + " - ");
-    numeros[i] = int.Parse(Console.ReadLine());
-}
+    Console.Clear();
 
-for (i = 0; i < 5; i++)
-{
-    for (j = 0; j < 5; j++)
+    Console.WriteLine("Vetor ORIGINAL:");
+    for (int i = 0; i < size; i++)
     {
-        if (numeros[j] > numeros[j + 1]) {
-            temp = numeros[j];
-            numeros[j] = numeros[j + 1];
-            numeros[j + 1] = temp;
+        //CRIANDO O VETOR ORIGINAL
+        original[i] = new Random().Next(1, 5);
+
+        if (i == 0)
+        {
+            ordenado[size_ordenado] = original[i];
+            size_ordenado++;
+        }
+        else
+        {
+            bool repete = false;
+            for (int j = 0; j < size; j++)
+            {
+                if (original[i] == ordenado[j])
+                {
+                    repete = true;
+                }
+            }
+            if (repete == false) {
+                ordenado[size_ordenado] = original[i];
+                size_ordenado++;
+            }
+        }
+
+
+        Console.Write($"{original[i]}  ");
+    }
+
+
+    //ORDENANDO
+    for (int referencia = 0; referencia < size_ordenado; referencia++)
+    {
+        for (int comparacao = referencia + 1; comparacao < size_ordenado; comparacao++)
+        {
+            if (ordenado[referencia] > ordenado[comparacao])
+            {
+                aux = ordenado[referencia];
+                ordenado[referencia] = ordenado[comparacao];
+                ordenado[comparacao] = aux;
+            }
         }
     }
-}
 
-Console.WriteLine("Vetor ordenado:");
-for (i = 0; i < 5; i++)
-{
-    Console.Write($"{numeros[i]}  ");
-}
+    Console.WriteLine("\n\nVetor ordenado utilizando BUBBLE SORT:");
+    for (int i = 0; i < size_ordenado; i++)
+    {
+        Console.Write($"{ordenado[i]}  ");
+    }
+
+
+    //INVERTENDO
+    //for (int referencia = size; referencia > 0; referencia--)
+    //{
+    //    for (int comparacao = referencia - 1; comparacao > 0; comparacao--)
+    //    {
+    //        if (ordenado[referencia] > ordenado[comparacao])
+    //        {
+    //            aux = ordenado[referencia];
+    //            ordenado[referencia] = ordenado[comparacao];
+    //            ordenado[comparacao] = aux;
+    //        }
+    //    }
+    //}
+
+    //Console.WriteLine("\n\nVetor invertido utilizando BUBBLE SORT:");
+    //for (int i = 0; i < size; i++)
+    //{
+    //    Console.Write($"{ordenado[i]}  ");
+    //}
+
+
+    Console.WriteLine("\n\nReiniciar o ordenador?");
+    Console.WriteLine("1 - SIM");
+    Console.WriteLine("2 - NÃO");
+    restart = int.Parse(Console.ReadLine());
+} while (restart == 1);
