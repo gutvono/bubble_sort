244f9e3409b5153f6f9e44602d75a4446c9b3d2b
Gustavo <guvono4@gmail.com>
2024-04-23 23:42:06 -0300

    update: agora também cria um novo vetor invertido para imprimir o valor invertido

--- bubble_sort/Program.cs
+++ bubble_sort/Program.cs
@@ -6,12 +6,13 @@ int[] invertido = new int[size];
 do
 {
     Console.Clear();
+    size_ordenado = 0;
 
     Console.WriteLine("Vetor ORIGINAL:");
     for (int i = 0; i < size; i++)
     {
         //CRIANDO O VETOR ORIGINAL
-        original[i] = new Random().Next(1, 5);
+        original[i] = new Random().Next(1, 81);
 
         if (i == 0)
         {
@@ -59,26 +60,32 @@ do
         Console.Write($"{ordenado[i]}  ");
     }
 
+    //COPIANDO O VETOR ORDENADO NO VETOR INVERTIDO
+    for (int i = 0; i < size_ordenado; i++) invertido[i] = ordenado[i];
 
     //INVERTENDO
-    //for (int referencia = size; referencia > 0; referencia--)
-    //{
-    //    for (int comparacao = referencia - 1; comparacao > 0; comparacao--)
-    //    {
-    //        if (ordenado[referencia] > ordenado[comparacao])
-    //        {
-    //            aux = ordenado[referencia];
-    //            ordenado[referencia] = ordenado[comparacao];
-    //            ordenado[comparacao] = aux;
-    //        }
-    //    }
-    //}
+    for (int referencia = 0; referencia < size_ordenado; referencia++)
+    {
+        for (int comparacao = referencia + 1; comparacao < size_ordenado; comparacao++)
+        {
+            if (invertido[referencia] < invertido[comparacao])
+            {
+                aux = invertido[referencia];
+                invertido[referencia] = invertido[comparacao];
+                invertido[comparacao] = aux;
+            }
+        }
+    }
 
-    //Console.WriteLine("\n\nVetor invertido utilizando BUBBLE SORT:");
-    //for (int i = 0; i < size; i++)
-    //{
-    //    Console.Write($"{ordenado[i]}  ");
-    //}
+    Console.WriteLine("\n\nVetor INVERTIDO com Bubble Sort:");
+    for (int i = 0; i < size_ordenado; i++)
+    {
+        Console.Write($"{invertido[i]}  ");
+    }
+
+    // 4 3 2 1
+    // 
+
 
 
     Console.WriteLine("\n\nReiniciar o ordenador?");
